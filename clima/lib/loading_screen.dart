771fc2a2d606a8f3c5edda2eebd27d8deb0cea2b
Vimarsh Pandey <vimarsh240501@gmail.com>
2025-03-18 15:24:50 +0530

    clima first commit

---
+++ clima/lib/loading_screen.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  String locationMessage = "Press the button to get location";
+
+  // Function to get location
+  Future<void> _getCurrentLocation() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // Check if location services are enabled
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      setState(() {
+        locationMessage = "Location services are disabled.";
+      });
+      return;
+    }
+
+    // Check permission status
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        setState(() {
+          locationMessage = "Location permission denied.";
+        });
+        return;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      setState(() {
+        locationMessage = "Permission denied forever. Enable manually.";
+      });
+      Geolocator.openAppSettings();
+      return;
+    }
+
+    // Get current location
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.low);
+
+    setState(() {
+      locationMessage =
+          "Latitude: ${position.latitude}, Longitude: ${position.longitude}";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Geolocator Example")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(locationMessage, style: TextStyle(color: Colors.white),),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _getCurrentLocation,
+              child: Text("Get Location"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
